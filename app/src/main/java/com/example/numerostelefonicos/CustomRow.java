98a4bf3b5b6663a088bcef707914350733607fec
Alejandro Rincón Vera <alejandro.rv97@gmail.com>
2020-04-07 21:45:08 -0500

    Data duplication corrected

--- app/src/main/java/com/example/numerostelefonicos/CustomRow.java
+++ app/src/main/java/com/example/numerostelefonicos/CustomRow.java
@@ -38,24 +38,41 @@ public class CustomRow extends BaseAdapter {
         return 0;
     }
 
+    private class ViewHolder {
+        TextView tv_name;
+        TextView tv_number;
+    };
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder = null;
 
         View customView = convertView;
 
         if(convertView == null){
             LayoutInflater layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            customView = layoutInflater.inflate(R.layout.custom_row, null);
+            customView = layoutInflater.inflate(R.layout.custom_row, null);// "Inflate the row with the customized design"
 
-            Contact contact = contactArrayList.get(position);
+            // Holder class to store each of the displayed views while scrolling
+            holder = new ViewHolder();
 
-            TextView tv_name = customView.findViewById(R.id.custom_tv_name);
-            tv_name.setText(contact.getName());
+            holder.tv_name = customView.findViewById(R.id.custom_tv_name);
+            holder.tv_number = customView.findViewById(R.id.custom_tv_number);
 
-            TextView tv_number = customView.findViewById(R.id.custom_tv_number);
-            tv_number.setText(contact.getNumber());
+            customView.setTag(holder); // the setTag method gets a single key to each customView (row) with the object reference
 
+        }else{
+            holder = (ViewHolder)customView.getTag(); // holder obtains the reference to its Holder objects
         }
+
+        Contact contact = contactArrayList.get(position); // Obtaining each contact from a Contact list
+
+        //TextView tv_name = customView.findViewById(R.id.custom_tv_name);
+        holder.tv_name.setText(contact.getName());
+
+        //TextView tv_number = customView.findViewById(R.id.custom_tv_number);
+        holder.tv_number.setText(contact.getNumber());
 
         return customView;
     }
